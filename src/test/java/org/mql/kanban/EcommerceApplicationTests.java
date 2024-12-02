bcb173c6c26eefbfcfd6c87992c4d858e15e93d9
MiftahAsmaa <miftahasmaa2020@gmail.com>
2024-12-02 23:42:52 +0100

    first commit

--- src/test/java/org/mql/kanban/EcommerceApplicationTests.java
+++ src/test/java/org/mql/kanban/EcommerceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.mql.ecommerce;
+package org.mql.kanban;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class KanbanApplicationTests {
 
 	@Test
 	void contextLoads() {
